0187d97cb4405d3ad97ffc483a7a8b541c3918a5
João Antunes <joao@zezam.io>
2024-03-26 16:34:02 +0000

    Experimenting multiple deploys

--- apps/app-one/src/app/app.tsx
+++ apps/app-one/src/app/app.tsx
@@ -2,7 +2,6 @@ export function App() {
   return (
     <div>
       <h1>App one deployed by CI</h1>
-      <h2>Firebase</h2>
       <h3>Firebase: app-one-nx</h3>
     </div>
   );
--- apps/app-two/src/app/app.tsx
+++ apps/app-two/src/app/app.tsx
@@ -2,7 +2,6 @@ export function App() {
   return (
     <div>
       <h1>App two deployed by CI</h1>
-      <h2>Firebase</h2>
       <h3>Firebase: app-two-nx</h3>
     </div>
   );
