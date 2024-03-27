221cea0735b9196960bdf78c5ad153213ed91674
João Antunes <joao@zezam.io>
2024-03-27 07:44:57 +0000

    Deploying static web app to azures through ci

--- apps/app-two/src/app/app.tsx
+++ apps/app-two/src/app/app.tsx
@@ -2,7 +2,8 @@ export function App() {
   return (
     <div>
       <h1>App two deployed by CI</h1>
-      <h3>Firebase: app-two-nx</h3>
+      <h2>Azure</h2>
+      <h3>Azure: app-two-nx</h3>
     </div>
   );
 }
