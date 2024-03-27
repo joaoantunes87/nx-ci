92cf95bc144a73399790aaabd6f175d5be6a8921
João Antunes <joao@zezam.io>
2024-03-27 14:41:13 +0000

    Proof of concept

--- apps/app-one/src/app/app.tsx
+++ apps/app-one/src/app/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <div>
-      <h1>App one deployed by CI</h1>
+      <h1>POC</h1>
       <h2>Azure</h2>
       <h3>Azure: app-one-nx</h3>
     </div>
--- apps/app-two/src/app/app.tsx
+++ apps/app-two/src/app/app.tsx
@@ -1,10 +1,9 @@
 export function App() {
   return (
     <div>
-      <h1>App two deployed by CI</h1>
+      <h1>POC</h1>
       <h2>Azure</h2>
       <h3>Azure: app-two-nx</h3>
-      <h4>Production</h4>
     </div>
   );
 }
