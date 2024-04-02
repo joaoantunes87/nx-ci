fce02f83b8a19d5447b86a2db7af794ec3a42e01
João Antunes <joao@zezam.io>
2024-04-02 10:35:23 +0100

    Experimenting with a new workflow for the whole pipeline

--- apps/app-one/src/app/app.tsx
+++ apps/app-one/src/app/app.tsx
@@ -4,7 +4,7 @@ export function App() {
       <h1>POC</h1>
       <h2>Azure</h2>
       <h3>Azure: app-one-nx</h3>
-      <h4>Domain: {import.meta.env.VITE_APP_ONE_DOMAIN}</h4>
+      <h4>Domain test: {import.meta.env.VITE_APP_ONE_DOMAIN}</h4>
     </div>
   );
 }
